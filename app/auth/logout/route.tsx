d73d78ebc8a745672612ae19c1211a4b76dbcb2c
Claudivan Filho <j.claudivan.filho@gmail.com>
2023-08-29 11:40:52 -0300

    optimize images

--- app/auth/logout/route.tsx
+++ app/auth/logout/route.tsx
@@ -3,8 +3,7 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  const requestUrl = new URL(request.url);
+export async function POST(_: Request) {
   const supabase = createRouteHandlerClient({ cookies });
 
   await supabase.auth.signOut();
