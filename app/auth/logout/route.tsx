08c36cd563e2951171593ece13e6b6c99067c769
Claudivan Filho <j.claudivan.filho@gmail.com>
2023-08-29 11:21:28 -0300

    fix logout room cookie

--- app/auth/logout/route.tsx
+++ app/auth/logout/route.tsx
@@ -1,3 +1,4 @@
+import { setSessionRoom } from "@/app/_utils/session";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
@@ -7,6 +8,7 @@ export async function POST(request: Request) {
   const supabase = createRouteHandlerClient({ cookies });
 
   await supabase.auth.signOut();
+  setSessionRoom(null);
 
   return NextResponse.json({ message: "Logout Successfully" });
 }
