0572b27776ff0bd7cfa71bb196b1799ab356ae37
Claudivan Filho <j.claudivan.filho@gmail.com>
2023-08-31 17:32:56 -0300

    supabase runner test

--- app/_utils/supabase.ts
+++ app/_utils/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("keyy", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, process.env);
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
